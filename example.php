9102cc1016a5471721a2c558f94bc050e6393085
the_hasanov <hikmet.hesenov.93@gmail.com>
2020-10-23 14:21:52 +0400

    laravel 8 support

--- example.php
+++ example.php
@@ -1,1 +1,9 @@
 <?php
+
+use Chameleon\Millikart\Operation\CreateOrder;
+
+Millikart::createOrder(function (CreateOrder $createOrder){
+    $createOrder->amount(10.0)
+        ->allUrl('http://example.com/payment/callback')
+        ->description('Order Description');
+});
